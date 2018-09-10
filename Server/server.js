f2ba60828b09880279c98d0c23c0f3391f9fef60
Babatunde <soft-sky@live.co.uk>
2018-09-10 17:58:47 +0100

    Fix heroku app error

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ import fileUpload from 'express-fileupload';
 import routes from './src/route/routes';
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
   app.use(morgan('short'));
 }
@@ -18,11 +18,15 @@ app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.text({ type: 'text/plain' }));
 app.use(bodyParser.raw({ type: '*/octet-stream' }));
 
+app.get('/', (req, res) => {
+  res.status(200)
+  .send(`<h1>Welcome...</h1><br><h4>Server is running on https://${req.hostname}:${port}/ `)
+  .end();
+});
 app.use('/api/v1', routes);
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
   app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}/`);
   });
 }
-
 export default app;
